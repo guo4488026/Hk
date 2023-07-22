f45c62697c23ba7397b394e98dbb20981435bf6a
gf4488026 <942541970@qq.com>
2023-07-22 15:39:58 +0800

    hiker upload

--- SrcJu.js
+++ SrcJu.js
@@ -1421,7 +1421,7 @@ function search(keyword, mode, sdata, group, type) {
                         //item.content = item.content || item.desc;
                         var erdesc = item.desc? "("+ item.desc +")":"";
                       
-                        item.desc = objmode=="sousuo"||objmode=="sousuopage"||objmode=="erji"  l? objdata.name+' · '+item.desc :objmode=="sousuotest"?(item.content || item.desc): (extra.desc || item.desc);
+                        item.desc = objmode=="sousuo"||objmode=="sousuopage"  ? MY_RULE.title+' · '+objdata.name+' · '+item.desc :objmode=="sousuotest"?(item.content || item.desc): (extra.desc || item.desc);
                          item.col_type = objmode=="sousuo"||objmode=="jusousuo"  ? "video" : (objmode=="sousuotest"||objmode=="sousuopage") ? "movie_1_vertical_pic" : "avatar";
                         resultdata.push(item);
                     }
