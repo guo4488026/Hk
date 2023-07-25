d519c12251ab01fdd097856e101cef6b1c0a261b
gf4488026 <942541970@qq.com>
2023-07-26 01:11:06 +0800

    hiker upload

--- SrcJu.js
+++ SrcJu.js
@@ -1008,7 +1008,7 @@ eval(JSON.parse(fetch("hiker://page/danmu")).rule);
                     url: url,
                     desc: 列表[i].desc,
                     img: 列表[i].img,
-                    col_type: extra.col_type?extra.col_type:list_col_type,
+                    col_type: 列表[i].col_type?列表[i].col_type:list_col_type,
                     extra: extra
                 });
             }
