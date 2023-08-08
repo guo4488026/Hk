2e1555a94541b566b09cbfbf2b0b4824dd3ec0b7
gf4488026 <942541970@qq.com>
2023-08-09 03:56:26 +0800

    hiker upload

--- SrcJu.js
+++ SrcJu.js
@@ -945,10 +945,10 @@ storage0.putMyVar("list",列表s);
                 extra.id = 列表[i].url;
                 extra.url = 列表[i].url;
                 extra.cls = "loadlist playlist";
-                if (stype != "正版") {
-                    extra.linkid = i;
-                    extra.name = name
-                }
+               
+                extra.linkid = i;
+                extra.name = name
+                
                 extra.jsLoadingInject = true;
                 let blockRules = ['.m4a', '.mp3', '.gif', '.jpeg', '.jpg', '.ico', '.png', 'hm.baidu.com', '/ads/*.js', 'cnzz.com', '51.la'];
                 if (extra.blockRules && $.type(extra.blockRules) == "array") {
