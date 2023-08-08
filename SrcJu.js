a640bfa9773d1c0b08c2c7eb4b6d3a982d8c28eb
gf4488026 <942541970@qq.com>
2023-08-08 20:58:40 +0800

    hiker upload

--- SrcJu.js
+++ SrcJu.js
@@ -407,8 +407,8 @@ function erji() {
             let itype;
             let 解析 = parse['解析'];
 
-           storage0.putMyVar("Lazy",解析);
-           log(storage0.getMyVar("Lazy"))
+           putMyVar("Lazy",解析);
+           log(getMyVar("Lazy"))
           
             lazy = $("").lazyRule((解析, 参数) => {
                 eval(JSON.parse(fetch("hiker://page/lazy")).rule)
