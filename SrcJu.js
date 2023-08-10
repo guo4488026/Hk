2256b5c8ecfc79391af867bd53cc00c7f7960434
gf4488026 <942541970@qq.com>
2023-08-10 17:01:46 +0800

    hiker upload

--- SrcJu.js
+++ SrcJu.js
@@ -506,8 +506,7 @@ putMyVar('sousuoPageType',input)
                 img: getItem("img_3", "http://pp.myapp.com/ma_icon/0/icon_42375936_1689215707/256"),
                 col_type: "icon_4",
                 extra: {
-                     newWindow: true,
-                     windowId: MY_RULE.title + "搜索",
+                     
                     longClick: [{
                        
                         title: "更换图标",
